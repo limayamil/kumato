882572f2c5be136462cc7d934eced924447901e4
limayamil <yamillues@gmail.com>
2020-11-16 01:29:40 -0300

    Actualizando versión, convertido a paradigma orientado a objetos, aún incompleto
    Detecta fin de ciclo de trabajo y descanso, pero falta el resto.

--- js/app.js
+++ js/app.js
@@ -4,15 +4,103 @@ const btnResume = document.getElementById("resume");
 const btnRest = document.getElementById("rest");
 const btnSkip = document.getElementById("skip");
 const divKumato = document.getElementById("kumato");
-const timer25 = 25;
+const timer25 = 0;
 const timerShortRest = 5;
-// 'Not Started' 'Stopped' 'Running' 'Awaiting'
-let timerStatus = "Not started";
-// 'Not started' 'Working' 'Resting'
-let kumatoStatus = "Not started";
-let awaitingPlay = false;
-let minutosRestantes = 0;
-let segundosRestantes = 0;
+const segundos = 60;
+
+class kumatoTimer {
+  constructor(status, timerStatus, esperandoPlay = false, minutosRestantes, segundosRestantes) {
+    this._status = status;
+    this._timerStatus = timerStatus;
+    this._esperandoPlay = esperandoPlay;
+    this._minutosRestantes = minutosRestantes;
+    this._segundosRestantes = segundosRestantes;
+  }
+
+  inicializar(){
+    this._status = "Working";
+    this._timerStatus = "Running";
+    this._esperandoPlay = true;
+    this._minutosRestantes = timer25;
+    this._segundosRestantes = segundos;
+  }
+
+  ejecutar(){
+    if (this._status == "Working") {
+      // Si Kumato se inicia por primera vez en la sesión.
+      this._timerStatus = "Running";
+      console.log('Ejecutando Kumato');
+      console.log(this._status);
+      divKumato.classList.remove("not-started");
+      divKumato.classList.add("running");
+      mostrarAccionesTrabajando();
+    } else if (this._status == "Resting") {
+      // Si un nuevo Kumato comienza luego de descansar.
+      console.log("Ejecutar");
+      this._timerStatus = "Running";
+      this._esperandoPlay = false;
+      divKumato.classList.remove("paused");
+      divKumato.classList.add("running");
+      mostrarAccionesDescansando();
+    }
+  }
+
+  pausar(){
+    this._timerStatus = "Stopped";
+    divKumato.classList.remove("running");
+    divKumato.classList.add("paused");
+    mostrarAccionesPausado();
+  }
+
+  reanudar(){
+    console.log("Estado actual: " + this._status);
+    this._timerStatus = "Running";
+    divKumato.classList.remove("paused");
+    divKumato.classList.add("running");
+    if (this._status == "Working"){
+      mostrarAccionesTrabajando();
+    } else if (this._status == "Resting") {
+      mostrarAccionesDescansando();
+    }
+  }
+
+  descansar(){
+    this._status = "Resting";
+    this._timerStatus = "Stopped";
+    this._esperandoPlay = true;
+    this._minutosRestantes = timerShortRest;
+    this._segundosRestantes = 0;
+    console.log(this._minutosRestantes);
+    console.log(this._segundosRestantes);
+    mostrarAccionesEsperando();
+    this.mostrarTimer();
+    divKumato.classList.remove("running");
+    divKumato.classList.add("resting");
+  }
+
+  saltearDescanso(){
+    this.status = "Working";
+    minutosRestantes = timerElegido - 1;
+    segundosRestantes = segundos;
+    mostrarAccionesTrabajando();
+  }
+
+  mostrarTimer(){
+    if (this._status == "Working" || this._status == "Resting") {
+      if (this._minutosRestantes.toString().length === 1) {
+        document.getElementById("minutos").innerHTML = "0" + this._minutosRestantes;
+      } else {
+        document.getElementById("minutos").innerHTML = this._minutosRestantes;
+      }
+      if (this._segundosRestantes.toString().length == 1) {
+        document.getElementById("segundos").innerHTML = "0" + this._segundosRestantes;
+      } else {
+        document.getElementById("segundos").innerHTML = this._segundosRestantes;
+      }
+    }
+  }
+}
+
 
 const ocultarBtn = (boton) => {
   if (boton.classList.contains("fadeIn")) {
@@ -37,12 +125,12 @@ const mostrarAccionesPorComenzar = () => {
 };
 
 const mostrarAccionesTrabajando = () => {
-  if (kumatoStatus == "Resting") {
+  if (kumato._status == "Resting") {
     ocultarBtn(btnStart);
     ocultarBtn(btnResume);
     mostrarBtn(btnPause);
     ocultarBtn(btnRest);
-  } else if (kumatoStatus == "Working") {
+  } else if (kumato._status == "Working") {
     ocultarBtn(btnStart);
     ocultarBtn(btnResume);
     mostrarBtn(btnPause);
@@ -63,118 +151,54 @@ const mostrarAccionesPausado = () => {
   mostrarBtn(btnResume);
 };
 
-const mostrarAccionesDescansando = () => {
-  ocultarBtn(btnPause);
+const mostrarAccionesDescansando = () => {  
   ocultarBtn(btnRest);
   ocultarBtn(btnResume);
+  ocultarBtn(btnStart);
+  mostrarBtn(btnPause);
   mostrarBtn(btnSkip);
 };
 
-const ejecutarKumato = (timerElegido) => {
-  if (kumatoStatus == "Not Started") {
-    divKumato.classList.remove("not-started");
-    divKumato.classList.add("running");
-    minutosRestantes = timerElegido - 1;
-    segundosRestantes = 60;
-    timerStatus = "Running";
-    kumatoStatus = "Working";
-    mostrarAccionesTrabajando();
-  } else if (awaitingPlay == true && kumatoStatus == "Resting") {
-    awaitingPlay = false;
-    divKumato.classList.remove("paused");
-    divKumato.classList.add("running");
-    mostrarAccionesDescansando();
-  }
-};
-
-const pausarKumato = () => {
-  timerStatus = "Stopped";
-  divKumato.classList.remove("running");
-  divKumato.classList.add("paused");
-  mostrarAccionesPausado();
-};
-
-const reanudarKumato = () => {
-  timerStatus = "Running";
-  console.log(timerStatus);
-  mostrarAccionesTrabajando();
-};
-
-const descansarKumato = () => {
-  kumatoStatus = "Resting";
-  minutosRestantes = timerShortRest - 1;
-  segundosRestantes = 60;
-  mostrarAccionesEsperando();
-  divKumato.classList.remove("running");
-  divKumato.classList.add("resting");
-  awaitingPlay = true;
-};
-
-const saltearDescansoKumato = () => {
-  kumatoStatus = "Working";
-  minutosRestantes = timerElegido - 1;
-  segundosRestantes = 60;
-  mostrarAccionesTrabajando();
-};
-
-const mostrarTimer = () => {
-  if (timerStatus === "Running") {
-    if (minutosRestantes.toString().length === 1) {
-      document.getElementById("minutos").innerHTML = "0" + minutosRestantes;
-    } else {
-      document.getElementById("minutos").innerHTML = minutosRestantes;
-    }
-    if (segundosRestantes.toString().length == 1) {
-      document.getElementById("segundos").innerHTML = "0" + segundosRestantes;
-    } else {
-      document.getElementById("segundos").innerHTML = segundosRestantes;
-    }
-  }
-};
-
 var actualizarTimer = setInterval(function () {
-  if (timerStatus === "Running" && awaitingPlay == false) {
-    if (minutosRestantes > 0) {
-      if (segundosRestantes > 0) {
-        segundosRestantes--;
+  if (kumato._timerStatus == "Running" && kumato._esperandoPlay == false) {
+    console.log('Kumato is ticking');
+    if (kumato._minutosRestantes > 0 || kumato._segundosRestantes > 0) {
+      if (kumato._segundosRestantes > 0) {
+        kumato._segundosRestantes--;
       } else {
-        minutosRestantes--;
-        segundosRestantes = 59;
+        kumato._minutosRestantes--;
+        kumato._segundosRestantes = segundos - 1;
       }
-      mostrarTimer();
+      kumato.mostrarTimer();
+    } else {
+      if (kumato._status == "Working"){
+        kumato.descansar();
+      } else if (kumato._status == "Resting"){
+        kumato.ejecutar();
+      }
     }
   }
 }, 1000);
 
 btnStart.addEventListener("click", () => {
-  ejecutarKumato(timer25);
+  kumato.ejecutar();
 });
 
 btnPause.addEventListener("click", () => {
-  pausarKumato();
+  kumato.pausar();
 });
 
 btnResume.addEventListener("click", () => {
-  reanudarKumato();
+  kumato.reanudar();
 });
 
 btnRest.addEventListener("click", () => {
-  descansarKumato();
+  kumato.descansar();
 });
 
 btnSkip.addEventListener("click", () => {
-  saltearDescansoKumato();
+  kumato.saltearDescanso();
 });
 
 mostrarAccionesPorComenzar();
-
-//ejecutarKumato(timer25);
-/*
-for(let m = timerElegido; m >= 0; m--){
-        for(let s = 60; s >= 0; s--){
-            console.log(s);
-            document.getElementById("segundos").innerHTML = s;
-        }
-        document.getElementById("minutos").innerHTML = m;
-    }
-    */
+var kumato = new kumatoTimer("Working", "Not Started", false, timer25, segundos)
